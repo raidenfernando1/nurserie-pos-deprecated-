f0ddc4013db46cc05803a68d7ae4bacf0a0f839f
Arteed <arteed19@gmail.com>
2025-09-23 09:40:15 +0800

    chore: changed heading text in 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 export default function NotFound() {
   return (
     <div className="h-full flex flex-col justify-center items-center px-10">
-      <h1 className="text-8xl tracking-wide p-5">nurserie.</h1>
+      <h1 className="text-5xl tracking-wide p-5">Something went wrong...</h1>
       <h2>404 - Not Found</h2>
       <p>The page you are looking for does not exist</p>
       <a href="/" className="flex gap-2 items-center py-10">
