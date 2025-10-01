0e3a4b76c9f2429ecc793d826e966a6e866081b0
Arteed <arteed19@gmail.com>
2025-10-01 14:09:02 +0800

    minor: Added Delete method api / added delete button cashier

---
+++ src/app/api/admin/cashier/[id]/route.ts
@@ -0,0 +1,57 @@
+import { NextResponse } from "next/server";
+import { db } from "@/lib/db-client";
+import { headers } from "next/headers";
+import auth from "@/lib/auth-server";
+
+export async function DELETE(
+    request: Request,
+    { params }: { params: { id: string } }
+) {
+    try {
+        const session = await auth.api.getSession({ headers: await headers() });
+
+        if (!session || session.user.role !== "admin") {
+            return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+        }
+
+        const adminID = session.user.id;
+        const cashierID = params.id;
+
+        if (!cashierID) {
+            return NextResponse.json({ error: "Cashier id is required" }, { status: 400 });
+
+        }
+
+        const result = await db`
+        DELETE FROM "user" 
+        WHERE id = ${cashierID}
+        AND admin_id = ${adminID}
+        AND role = 'cashier'
+        RETURNING id, name `;
+
+        if (result.length === 0) {
+            return NextResponse.json({ success: false, error: "Cashier not found or not authorized" }, { status: 404 });
+        }
+
+        console.log(`Cashier ${cashierID} deleted by admin ${adminID}`);
+
+        return NextResponse.json({
+            success: true,
+            message: "Cashier deleted successfully",
+            deleteCashierId: result[0],
+        },
+            { status: 200 }
+        );
+
+    } catch (error) {
+        console.log(">>> Error in DELETE /api/admin/cashier/[id]", error);
+        return NextResponse.json(
+            {
+                success: false,
+                error: "Failed to delete cashier",
+                details: error instanceof Error ? error.message : "Unknown error",
+            },
+            { status: 500 }
+        );
+    }
+}
